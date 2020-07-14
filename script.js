fb27879c2ecc296a1b595eb6caf2eab4eba8c275
Mona <66236313+xelinx@users.noreply.github.com>
2020-07-14 00:53:43 -0700

    add savebtn function

--- script.js
+++ script.js
@@ -3,4 +3,17 @@ $(document).ready(function(){
     //display current Date, MMDDYYYY, HH:MM am/pm
     $("#currentDay").text(moment().format('dddd, MMMM Do'));
 
+
+    //Define save button
+    var saveBtn = $(".saveBtn")
+
+    //Save button stores data in local storage
+    saveBtn.addEventListener("click", function (){
+        var time = $(this).parent().attr("id");
+        var schedule = $(this).siblings(".schedule").val();
+    
+        localStorage.setItem(time, schedule);
+    });
+
+
 });
